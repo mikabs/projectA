3a70a3163ead6356d21e54465731a547da3f5825
Mika bs <144587909+mikabs@users.noreply.github.com>
2023-10-17 12:59:50 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,57 @@
+/* class Main {
+  public static void main(String[] args) {
+    int x = 4, y = 6, z = 8;
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+  }
+} 
+*/
+
+/* class Main {
+  public static void main(String[] args) {
+    float x = 1.5f;
+    int y = 2;
+    System.out.println(x + "   " + y) ;
+  }
+} */
+
+/* class Main {
+  public static void main(String[] args) {
+  char x = 'M';
+  char y = 'i';
+  char z = 'k';
+  char w = 'a';
+  System.out.println(x+""+y+""+z+""+w);
+  }
+} */
+
+/* class Main {
+  public static void main(String[] args) {
+  int x = 1, y = 4;
+ System.out.println(x+ " is less than "+y);
+ } */
+
+/* class Main {
+  public static void main(String[] args) {
+  int x = 2, y = 10, z = 3;
+  float average = (float)(x+y+z)/3;
+  System.out.println(average);
+  }
+} */
+
+/* class Main {
+  public static void main(String[] args) {
+    int x = 4, y = 2;
+    System.out.println(x*x+y*y);
+  }
+} */
+
+ /* class Main {
+  public static void main(String[] args) {
+  int x = 2;
+  int y = (2*x+5);
+    System.out.println("y=2x=5");
+    System.out.println("when x=2 y="+y);
+ } 
+} */
